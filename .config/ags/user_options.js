07ae14e14f263447dd76541cc145e48688107e48
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2024-03-06 17:55:14 -0500

    Update user_options.js

--- .config/ags/user_options.js
+++ .config/ags/user_options.js
@@ -44,9 +44,9 @@ let userConfigOptions = {
         // For seconds, add "%S" and set interval to 1000
         'format': "%H:%M",
         'interval': 5000,
-        'dateFormatLong': "%A, %d/%m", // On bar
+        'dateFormatLong': "%A, %m/%d", // On bar
         'dateInterval': 5000,
-        'dateFormat': "%d/%m", // On notif time
+        'dateFormat': "%m/%d", // On notif time
     },
     'weather': {
         'city': "",
