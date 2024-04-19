b88ee3f02d90599358cb6023c82ecd3d4150feff
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2024-04-18 21:39:11 -0400

    Updated user_options

--- .config/ags/user_options.js
+++ .config/ags/user_options.js
@@ -56,6 +56,12 @@ let userConfigOptions = {
     },
     // Longer stuff
     'icons': {
+        // Find the window's icon by its class with levenshteinDistance
+        // The file names are processed at startup, so if there
+        // are too many files in the search path it'll affect performance
+        // Example: ['/usr/share/icons/Tela-nord/scalable/apps']
+        'searchPaths': [''],
+
         substitutions: {
             'code-url-handler': "visual-studio-code",
             'Code': "visual-studio-code",
@@ -74,6 +80,26 @@ let userConfigOptions = {
             'nextTab': "Ctrl+Page_Down",
             'prevTab': "Ctrl+Page_Up",
         },
+    },
+    'dock': {
+        'enabled': true,
+        'hiddenThickness': 5,
+        'pinnedApps': ['brave','foot'],
+        'layer': 'top',
+        'monitorExclusivity': true, // Dock will move to other monitor along with focus if enabled
+        'searchPinnedAppIcons': false, // Try to search for the correct icon if the app class isn't an icon name
+        'trigger': [], // client_added, client_move, workspace_active, client_active
+        // Automatically hide dock after `interval` ms since trigger
+        'autoHide': [
+            {
+                'trigger': 'client-added',
+                'interval': 500,
+            },
+            {
+                'trigger': 'client-removed',
+                'interval': 500,
+            },
+        ],
     },
 }
 
