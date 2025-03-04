cbdd8deb794296b7f2e9db3307da9afea29b0ea0
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2025-03-04 17:14:21 -0500

    copy share to share

--- install.sh
+++ install.sh
@@ -251,6 +251,7 @@ esac
 # some foldes (eg. .local/bin) should be processed separately to avoid `--delete' for rsync,
 # since the files here come from different places, not only about one program.
 v rsync -av ".local/bin/" "$XDG_BIN_HOME"
+v rsync -av ".local/share/" "$XDG_DATA_HOME"
 
 # Prevent hyprland from not fully loaded
 sleep 1
