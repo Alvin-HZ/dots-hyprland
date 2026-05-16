7273b69c02db7f29d714f6b0ca3e4a90f39eff5e
Alvin <153151121+Alvin-HZ@users.noreply.github.com>
2026-05-16 11:23:17 -0400

    add device.lua

--- dots/.config/hypr/hyprland.lua
+++ dots/.config/hypr/hyprland.lua
@@ -31,6 +31,9 @@ end
 if is_file_exists(HOME .. "/.config/hypr/custom/keybinds.lua") then
     require("custom.keybinds")
 end
+if is_file_exists(HOME .. "/.config/hypr/custom/device.lua") then
+    require("custom.device")
+end
 
 -- nwg-displays support --
 if is_file_exists(HOME .. "/.config/hypr/workspaces.lua") then
